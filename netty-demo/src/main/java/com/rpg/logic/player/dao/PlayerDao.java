7c1eb046bdbf366d0eae99d7be628cbfda0fffb9
wudeji <YW0916@YW-01B-0523.corp.yaowan.com>
2018-05-12 10:11:06 +0800

    修改dao为注解方式

--- netty-demo/src/main/java/com/rpg/logic/player/dao/PlayerDao.java
+++ netty-demo/src/main/java/com/rpg/logic/player/dao/PlayerDao.java
@@ -1,12 +1,18 @@
 package com.rpg.logic.player.dao;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
 import com.rpg.logic.player.domain.Player;
 
 public interface PlayerDao {
 
+	@Insert(" insert into Player(playerId,name,user,senceId) values (#{playerId},#{name},#{user},#{senceId}) ")
 	public int createPlayer(Player player);
 	
+	@Select("select * from Player where user=#{user}")
 	public Player getByUser(String user);
 	
+	@Select("select * from Player where playerId=#{playerId}")
 	public Player getByPlayerId(int playerId);
 }
--- netty-demo/src/main/java/com/rpg/logic/player/dao/UserDao.java
+++ netty-demo/src/main/java/com/rpg/logic/player/dao/UserDao.java
@@ -1,8 +1,11 @@
 package com.rpg.logic.player.dao;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.rpg.logic.player.domain.User;
 
 public interface UserDao {
 
+	@Select("select * from user where id=#{id}")
 	public User getById(int id);
 }
