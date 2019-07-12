99cf6de532eb05238ba6e05af3222d63bdeaf087
wiseks <wiseks@hotmail.com>
2019-07-12 16:10:29 +0800

    hash

--- netty-framework/src/main/java/com/rpg/framework/executor/GameExecutorService.java
+++ netty-framework/src/main/java/com/rpg/framework/executor/GameExecutorService.java
@@ -15,34 +15,43 @@ import com.rpg.framework.config.ServerConfig;
 
 @Service
 public class GameExecutorService {
-	
+
 	@Autowired
 	private ServerConfig serverConfig;
 
 	private ExecutorService[] services;
-	
+
 	private AtomicInteger index = new AtomicInteger();
-	
-	public Future<?> execute(Object object, Runnable runnable) {
-		int hash = object.hashCode();
+
+	public Future<?> execute(Object id, Runnable runnable) {
+		int hash = hash(id.hashCode());
 		int threadIndex = hash % services.length;
 		ExecutorService service = services[threadIndex];
 		return service.submit(runnable);
 	}
-	
+
+	private static int hash(int h) {
+		h += (h << 15) ^ 0xffffcd7d;
+		h ^= (h >>> 10);
+		h += (h << 3);
+		h ^= (h >>> 6);
+		h += (h << 2) + (h << 14);
+		return h ^ (h >>> 16);
+	}
+
 	@PostConstruct
 	private void init() {
 		services = new ExecutorService[serverConfig.getThreadNum()];
 //		for(int i=0;i<services.length;i++){
 //			services[i] = Executors.newSingleThreadExecutor();
 //		}
-		
-		for(int i= 0;i < services.length; i++){
+
+		for (int i = 0; i < services.length; i++) {
 			services[i] = Executors.newSingleThreadExecutor(new ThreadFactory() {
-				
+
 				@Override
 				public Thread newThread(Runnable r) {
-					return new Thread("gameHandlerPool-thread-"+index.incrementAndGet());
+					return new Thread("gameHandlerPool-thread-" + index.incrementAndGet());
 				}
 			});
 		}
