b1559b6e8724ad8f5ad78b7e41ed7384919dcf4a
wiseks <wiseks@hotmail.com>
2019-07-12 21:23:54 +0800

    格式化

--- netty-framework/src/main/java/com/rpg/framework/handler/dispatcher/DispatcherEvent.java
+++ netty-framework/src/main/java/com/rpg/framework/handler/dispatcher/DispatcherEvent.java
@@ -5,22 +5,19 @@ import com.rpg.framework.code.Request;
 import io.netty.channel.ChannelHandlerContext;
 
 public class DispatcherEvent {
-	
+
 	private ChannelHandlerContext context;
 
 	private Request request;
-	
 
 	public DispatcherEvent() {
-		
+
 	}
 
 	public DispatcherEvent(ChannelHandlerContext context, Request request) {
 		this.context = context;
 		this.request = request;
 	}
-	
-	
 
 	public Request getRequest() {
 		return request;
@@ -37,5 +34,5 @@ public class DispatcherEvent {
 	public void setContext(ChannelHandlerContext context) {
 		this.context = context;
 	}
-	
+
 }
